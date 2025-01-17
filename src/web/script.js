0e8051a7e630d22a1a79a83b84fc6b2d1220ad02
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-01-17 22:56:14 +0700

    add preview csv file content in web

--- src/web/script.js
+++ src/web/script.js
@@ -1,8 +1,45 @@
 const dataForm = document.getElementById('data-form')
+const previewData = document.getElementById('preview-data-container')
 
 dataForm.addEventListener('submit', function(e) {
     e.preventDefault()
 
-    console.log('data!')
-    console.log(this)
+    const formData = new FormData(this)
+    const file = formData.get('data')
+
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+        const fileContent = e.target.result
+        const rows = fileContent.split(/[\r\n]+/).map(row => row.split(','))
+        previewData.style.display = 'block'
+        
+        let previewedTable = `
+        <table class="min-w-full table-auto bg-white shadow-md rounded-lg">
+            <thead class="bg-gray-200 rounded-t-lg">
+                <tr>
+        `
+
+        for (const item of rows.shift()) {
+            previewedTable += `<td class="px-4 py-2 text-left text-sm font-semibold text-gray-700">${item}</td>`
+        }
+
+        previewedTable += `
+                </tr>
+            </thead>
+        `
+
+        for (const row of rows) {
+            previewedTable += `<tr>`
+            for (const item of row) {
+                previewedTable += `<td>${item}</td>`
+            }
+            previewedTable += `</tr>`
+
+        }
+
+        previewData.innerHTML = previewedTable
+    }
+
+    reader.readAsText(file)
 })
